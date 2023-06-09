d12db02f479a95ba7f34d024bb3c8787296f25ea
oman <omanshah76@gmail.com>
2023-06-09 15:36:17 +0500

    O

---
+++ NetBeansProjects/JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -0,0 +1,46 @@
+
+package javaapplication4;
+import java.util.Scanner;
+public class JavaApplication4 
+{
+    public static void main(String[] args) 
+    { 
+       Scanner sca = new Scanner(System.in);
+       
+       String ch="a";
+       String a=sca.nextLine();
+       
+        System.out.println("hello woerld");
+//       switch(ch){
+//           case"a":
+//               System.out.println((char)1575);
+//           
+//           
+//       }
+
+//        System.out.println("Enter your salary here");
+//        int Salary = sca.nextInt();
+//        
+//        if(Salary>=10000 && Salary<30000)
+//        {
+//            System.out.println("Your 25% bonus is =" +Salary/100*25);
+//            int Total = Salary + Salary/100*25;
+//            System.out.println("Hete is your is  =" +Total);
+//        }
+//       
+//        else if( Salary>=30000 && Salary<=50000)
+//        {
+//            System.out.println("Your 10% bonus is =" +Salary/100*10);
+//        
+//            int Total = Salary + Salary/100*10;
+//            System.out.println("Hete is your is  =" +Total);
+//        }
+//        
+//        else
+//        {
+//            System.out.println("Not eligible for any bonus");
+//        }
+//        
+    }
+    
+}
